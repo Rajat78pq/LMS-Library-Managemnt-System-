ecfb9d89c0b908f7e72bbd84e11fcb633abe19ae
rajat78pq <rajatkumarjena767@gmail.com>
2024-04-08 21:03:19 +0530

    Add issue Book Code

--- backend/src/BookOperation/AddBook.ts
+++ backend/src/BookOperation/AddBook.ts
@@ -8,7 +8,7 @@ const AddBook =async(req:Request,res:Response)=>{
         const result = await client.query(`INSERT INTO public."Book"(
             "Registration", "Branch", "RollNo", "BookName", "Author", "Date")
             VALUES ($1, $2, $3, $4, $5, $6)`,[Registration,Branch,RollNo,BookName,Author,Date]);
-            res.json({Message:"Data has been Insert"})
+            res.json({succes:true, Message:"Data has been Insert"})
     } catch (error) {
         console.log(`error from book ${error}`)
     }
--- frontend/src/issueBook/issueBook.tsx
+++ frontend/src/issueBook/issueBook.tsx
@@ -11,9 +11,17 @@ const IssueBook = () => {
   const [Date, setDate] = useState('');
 
 
-  const AddBooks=(e:any)=>{
+  const AddBooks = async(e: any) => {
     e.preventDefault();
-    axios.post('')
+
+    try {
+    const Response = await axios.post(`http://localhost:2000/add-book`, { Registration: Registration, Branch: Branch, RollNo: RollNo, BookName: Book, Author: Author, Date: Date })
+      if(Response.data.succes == true){
+        alert("Data Stored");
+      }
+    } catch (error) {
+      console.log(`Error from frontend ${error}`);
+    }
   }
 
 
@@ -30,21 +38,21 @@ const IssueBook = () => {
             <div className="form">
               <div className="form-style">
                 <label htmlFor="Registration">Enter Registration No</label>
-                <input onChange={(e)=>{setRegistration(e.target.value)}} type="text" placeholder='Registration No.' required />
+                <input onChange={(e) => { setRegistration(e.target.value) }} type="text" placeholder='Registration No.' required />
                 <label htmlFor="Branch">Enter Branch</label>
-                <input onChange={(e)=>{setBranch(e.target.value)}} type="text" placeholder='Branch' required />
+                <input onChange={(e) => { setBranch(e.target.value) }} type="text" placeholder='Branch' required />
               </div>
               <div className="form-style">
                 <label htmlFor="RollNo">Roll No</label>
-                <input onChange={(e)=>{setRollNo(e.target.value)}} type="text" placeholder='Roll no' required />
+                <input onChange={(e) => { setRollNo(e.target.value) }} type="text" placeholder='Roll no' required />
                 <label htmlFor="Book Name">Book Name</label>
-                <input onChange={(e)=>{setBook(e.target.value)}} type="text" placeholder='Book Name' required />
+                <input onChange={(e) => { setBook(e.target.value) }} type="text" placeholder='Book Name' required />
               </div>
               <div className="form-style">
                 <label htmlFor="Author">Author Name</label>
-                <input onChange={(e)=>{setAuthor(e.target.value)}} type="text" placeholder='Author' required />
+                <input onChange={(e) => { setAuthor(e.target.value) }} type="text" placeholder='Author' required />
                 <label htmlFor="Issue Date">Issue Date</label>
-                <input onChange={(e)=>{setDate(e.target.value)}} type="date" name="Issue Date" id="issueDAte" required />
+                <input onChange={(e) => { setDate(e.target.value) }} type="date" name="Issue Date" id="issueDAte" required />
               </div>
               <button onClick={AddBooks}>Add</button>
             </div>
